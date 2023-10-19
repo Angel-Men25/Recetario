b073848e12913bf6dca5af4da3e0fa258cae5109
Angel-Men25 <angel.valdez.25@hotmail.com>
2023-10-19 17:08:36 -0600

    Agrega normalize.css, modifica index.html, style.css y app.js

--- js/app.js
+++ js/app.js
@@ -3,14 +3,137 @@
 // The Meal DB 
 // https://www.themealdb.com/api.php
 
-document.addEventListener('DOMContentLoaded', fetchData);
+function app() {
+  // VARIABLES
+  const divContainer = document.querySelector('.container');
+  const sectionFood = document.querySelector('.section');
 
+  // document.addEventListener('DOMContentLoaded', fetchData);
+  fetchData();
 
-async function fetchData() {
-  try {
-    const url = ''
-    const response = await fetch('')
-  } catch (error) {
-    console.log(error);
+  async function fetchData() {
+    try {
+      const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+      const response = await fetch(url);
+      // console.log(response);
+      if (response.status === 404) {
+        // SORRY PAGE NOT FOUND
+      } else {
+        // PAGINA FUNCIONA
+        const data = await response.json();
+        // console.log(data.categories);
+        showCategories(data.categories);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  function showCategories(categories) {
+    categories.forEach(category => {
+      const { idCategory, strCategory, strCategoryThumb } = category;
+      // DIV
+      const platillo = document.createElement('button');
+      platillo.classList.add('platillo');
+      platillo.dataset.id = strCategory;
+
+      // IMG
+      const imgPlatillo = document.createElement('img');
+      imgPlatillo.src = strCategoryThumb;
+      imgPlatillo.classList.add('img');
+
+      // TITLE
+      const titlePlatillo = document.createElement('h2');
+      titlePlatillo.classList.add('title');
+      titlePlatillo.innerText = strCategory;
+
+
+      platillo.appendChild(imgPlatillo);
+      platillo.appendChild(titlePlatillo);
+
+      divContainer.appendChild(platillo);
+    });
+  }
+
+  divContainer.addEventListener('click', showTarget);
+
+  function showTarget(e) {
+    const namePlatillo = e.target.parentElement.getAttribute('data-id');
+    console.log(namePlatillo);
+    fetchDish(namePlatillo);
+  }
+
+  async function fetchDish(namePlatillo) {
+    try {
+      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${namePlatillo}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      showDishContainer(data.meals);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function showDishContainer(meals) {
+
+    // Limpiar HTML
+    limpiarHTML();
+
+    meals.forEach(meal => {
+      const { idMeal, strMeal, strMealThumb } = meal
+      // console.log(meal);
+      // ESCRIBIR HTML PARA VER COMO SE MOSTRARA CADA PLATILLO
+      const aPlatillo = document.createElement('a');
+      aPlatillo.classList.add('platillo__link');
+      aPlatillo.href = `receta.html?id=${idMeal}`;
+      aPlatillo.target = '_blank';
+      // aPlatillo.dataset.id = idMeal;
+      // aPlatillo.onclick = (e) => {
+      //   const id = e.target.parentElement.getAttribute('data-id');
+      //   console.log(id);
+      //   fetchDescription(id);
+      // }
+
+      // IMG
+      const imgPlatillo = document.createElement('img');
+      imgPlatillo.src = strMealThumb;
+      imgPlatillo.classList.add('img');
+
+      // TITLE
+      const titlePlatillo = document.createElement('h2');
+      titlePlatillo.classList.add('title');
+      titlePlatillo.innerText = strMeal;
+
+      aPlatillo.appendChild(imgPlatillo);
+      aPlatillo.appendChild(titlePlatillo);
+
+      sectionFood.appendChild(aPlatillo);
+    });
+  }
+
+  // async function fetchDescription(id) {
+  //   try {
+  //     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(data.meals);
+  //     showDescription(data.meals);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // function showDescription(description = []) {
+  //   console.log(description);
+  // }
+
+  function limpiarHTML() {
+    while (sectionFood.firstChild) {
+      sectionFood.removeChild(sectionFood.firstChild);
+    }
+  }
+
+
 }
+
+document.addEventListener('DOMContentLoaded', app);
