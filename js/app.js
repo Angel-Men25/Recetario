4dda2b433ed209e10428d418dcee09e4fad6e863
Angel-Men25 <angel.valdez.25@hotmail.com>
2023-10-19 10:11:34 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// The Cocktail DB 
+// https://www.thecocktaildb.com/api.php
+// The Meal DB 
+// https://www.themealdb.com/api.php
+
+document.addEventListener('DOMContentLoaded', fetchData);
+
+
+async function fetchData() {
+  try {
+    const url = ''
+    const response = await fetch('')
+  } catch (error) {
+    console.log(error);
+  }
+}
